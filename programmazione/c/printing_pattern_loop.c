2b6ccfe960465c677ca8b471ec37d40ec260310e
--leo <162900279+subnetMusk@users.noreply.github.com>
2024-06-21 12:31:19 +0200

    Create printing_pattern_loop.c

---
+++ programmazione/c/printing_pattern_loop.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void stampa_array(int d, int a[d]){
+    for(int i = 0; i < d; i++){
+        printf("%d", a[i]);
+        if(i != d - 1)
+            printf(" ");
+    }
+    return;
+}
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+
+  	// Complete the code to print the pattern.
+    int dim = (n * 2) - 1;
+    int array[dim];
+
+    for(int i = 0; i < (n * 2) - 1; i++){
+        for(int j = 0; j < (n * 2) - 1; j++){
+            if(i == 0 || i == (n * 2) - 2){
+                for(int i = 0; i < dim; i++){
+                    array[i] = n;
+                }
+            }
+            else{
+                if(j >= i)
+                    array[j]--;
+                if(j >= (n * 2 - 1) - i)
+                    array[j]++;
+            }
+        }
+        stampa_array(dim, array);
+        printf("\n");
+    }
+
+    return 0;
+}
