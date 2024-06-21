5009f892be3297ca7865b44ac1e6c03da85a5091
--leo <162900279+subnetMusk@users.noreply.github.com>
2024-06-21 12:26:23 +0200

    Update stringhe.h

--- programmazione/c/stringhe/stringhe.h
+++ programmazione/c/stringhe/stringhe.h
@@ -2,6 +2,7 @@
 void stampaStringa(char* s);
 int lung_string(char* s);
 int lung_string_r(char* s);
+void strcat_c (char *str, char c);
 void inverti_stringa(char *s, int dim)
 void concatenaStringhe(char* S1, char* S2);
 void test_concatenaStringhe();
