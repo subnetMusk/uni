b732dc0cdd3db27a404c82c2e592dc52911ae218
--leo <162900279+subnetMusk@users.noreply.github.com>
2024-06-21 11:43:31 +0200

    Update array.h

--- programmazione/c/array/array.h
+++ programmazione/c/array/array.h
@@ -1,6 +1,7 @@
 #include "array/read_only.c"
 void leggi_array(int a[], int dim);
 void stampa_array(int dim, int a[dim]);
+void stampaMatrice(int *p, int righe, int colonne);
 void swap(int a[], int i1, int i2);
 int partition(int a[], int lo, int hi);
 void quicksort(int a[], int lo, int hi); //lo = indice minore; hi = indice maggiore
